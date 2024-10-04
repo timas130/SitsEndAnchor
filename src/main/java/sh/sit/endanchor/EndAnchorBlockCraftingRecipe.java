936c61b4a9d540c3f2a3522f8f380f9a5eb65851
sit <me@sit.sh>
2024-10-04 16:31:16 +0300

    allow customizing the crafting recipe

--- src/main/java/sh/sit/endanchor/EndAnchorBlockCraftingRecipe.java
+++ src/main/java/sh/sit/endanchor/EndAnchorBlockCraftingRecipe.java
@@ -1,56 +1,83 @@
 package sh.sit.endanchor;
 
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.fabricmc.loader.impl.util.log.Log;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.LodestoneTrackerComponent;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.network.RegistryByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.recipe.CraftingRecipe;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.recipe.RawShapedRecipe;
 import net.minecraft.recipe.RecipeSerializer;
-import net.minecraft.recipe.SpecialCraftingRecipe;
 import net.minecraft.recipe.book.CraftingRecipeCategory;
 import net.minecraft.recipe.input.CraftingRecipeInput;
 import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.GlobalPos;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
 
-public class EndAnchorBlockCraftingRecipe extends SpecialCraftingRecipe {
-    private static final Item[] INGREDIENTS = {
-            Items.OBSIDIAN, Items.POPPED_CHORUS_FRUIT, Items.OBSIDIAN,
-            Items.POPPED_CHORUS_FRUIT, Items.COMPASS, Items.POPPED_CHORUS_FRUIT,
-            Items.OBSIDIAN, Items.POPPED_CHORUS_FRUIT, Items.OBSIDIAN
-    };
+public class EndAnchorBlockCraftingRecipe implements CraftingRecipe {
+    final String group;
+    final CraftingRecipeCategory category;
+    final RawShapedRecipe raw;
+    final boolean showNotification;
 
-    public EndAnchorBlockCraftingRecipe(CraftingRecipeCategory category) {
-        super(category);
+    public EndAnchorBlockCraftingRecipe(String group,
+                                        CraftingRecipeCategory category,
+                                        RawShapedRecipe raw,
+                                        boolean showNotification) {
+        this.group = group;
+        this.category = category;
+        this.raw = raw;
+        this.showNotification = showNotification;
     }
 
     @Override
     public boolean matches(CraftingRecipeInput input, World world) {
-        // require full-sized crafting table
-        if (input.getStackCount() != 9) {
-            return false;
-        }
+        if (!raw.matches(input)) return false;
 
-        // check ingredients
-        for (int i = 0; i < 9; i++) {
-            if (!input.getStackInSlot(i).isOf(INGREDIENTS[i])) {
-                return false;
-            }
-        }
+        // find the lodestone compass in the recipe
+        ItemStack lodestoneCompassStack = findLodestoneCompass(input);
+        if (lodestoneCompassStack == null) return false;
 
-        // require compass to be a lodestone compass
-        final ItemStack lodestoneCompassStack = input.getStackInSlot(4);
-        final LodestoneTrackerComponent lodestoneTrackerComponent = lodestoneCompassStack.get(DataComponentTypes.LODESTONE_TRACKER);
-        if (lodestoneTrackerComponent == null) {
-            return false;
-        }
+        // check the position the compass is pointing to
+        LodestoneTrackerComponent lodestoneTrackerComponent = lodestoneCompassStack.get(DataComponentTypes.LODESTONE_TRACKER);
+        assert lodestoneTrackerComponent != null;
 
         final GlobalPos lodestonePos = lodestoneTrackerComponent.target().orElse(null);
         if (lodestonePos == null) {
             return false;
         }
+
         // ensure compass points to the end
         return lodestonePos.dimension() == World.END;
+    }
+
+    private static @Nullable ItemStack findLodestoneCompass(CraftingRecipeInput input) {
+        ItemStack lodestoneCompassStack = null;
+        for (int i = 0; i < input.getSize(); i++) {
+            final ItemStack stack = input.getStackInSlot(i);
+            if (stack.isOf(Items.COMPASS) && stack.get(DataComponentTypes.LODESTONE_TRACKER) != null) {
+                if (lodestoneCompassStack != null) {
+                    Log.info(SitsEndAnchor.LOG_CATEGORY, "Invalid end anchor recipe: multiple lodestone compasses");
+                    return null;
+                }
+                lodestoneCompassStack = stack;
+            }
+        }
+
+        if (lodestoneCompassStack == null) {
+            Log.info(SitsEndAnchor.LOG_CATEGORY, "Invalid end anchor recipe: no lodestone compass");
+            return null;
+        }
+
+        return lodestoneCompassStack;
     }
 
     @Override
@@ -60,7 +87,8 @@ public class EndAnchorBlockCraftingRecipe extends SpecialCraftingRecipe {
             return ItemStack.EMPTY;
         }
 
-        final ItemStack lodestoneCompassStack = input.getStackInSlot(4);
+        final ItemStack lodestoneCompassStack = findLodestoneCompass(input);
+        assert lodestoneCompassStack != null; // checked in matches()
         final LodestoneTrackerComponent lodestoneTrackerComponent = lodestoneCompassStack.get(DataComponentTypes.LODESTONE_TRACKER);
         assert lodestoneTrackerComponent != null; // checked in matches()
 
@@ -75,11 +103,71 @@ public class EndAnchorBlockCraftingRecipe extends SpecialCraftingRecipe {
 
     @Override
     public boolean fits(int width, int height) {
-        return width == 3 && height == 3;
+        return width >= raw.getHeight() && height >= raw.getHeight();
+    }
+
+    @Override
+    public ItemStack getResult(RegistryWrapper.WrapperLookup registriesLookup) {
+        return new ItemStack(SitsEndAnchor.END_ANCHOR_BLOCK_ITEM);
     }
 
     @Override
     public RecipeSerializer<EndAnchorBlockCraftingRecipe> getSerializer() {
         return SitsEndAnchor.END_ANCHOR_BLOCK_CRAFTING_RECIPE;
+    }
+
+    @Override
+    public CraftingRecipeCategory getCategory() {
+        return category;
+    }
+
+    @Override
+    public boolean showNotification() {
+        return showNotification;
+    }
+
+    @Override
+    public DefaultedList<Ingredient> getIngredients() {
+        return raw.getIngredients();
+    }
+
+    public static class Serializer implements RecipeSerializer<EndAnchorBlockCraftingRecipe> {
+        public static final MapCodec<EndAnchorBlockCraftingRecipe> CODEC = RecordCodecBuilder.mapCodec(
+                instance -> instance.group(
+                                Codec.STRING.optionalFieldOf("group", "").forGetter(recipe -> recipe.group),
+                                CraftingRecipeCategory.CODEC.fieldOf("category").orElse(CraftingRecipeCategory.MISC).forGetter(recipe -> recipe.category),
+                                RawShapedRecipe.CODEC.forGetter(recipe -> recipe.raw),
+                                Codec.BOOL.optionalFieldOf("show_notification", Boolean.TRUE).forGetter(recipe -> recipe.showNotification)
+                        )
+                        .apply(instance, EndAnchorBlockCraftingRecipe::new)
+        );
+        public static final PacketCodec<RegistryByteBuf, EndAnchorBlockCraftingRecipe> PACKET_CODEC = PacketCodec.ofStatic(
+                Serializer::write, Serializer::read
+        );
+
+        @Override
+        public MapCodec<EndAnchorBlockCraftingRecipe> codec() {
+            return CODEC;
+        }
+
+        @Override
+        public PacketCodec<RegistryByteBuf, EndAnchorBlockCraftingRecipe> packetCodec() {
+            return PACKET_CODEC;
+        }
+
+        private static EndAnchorBlockCraftingRecipe read(RegistryByteBuf buf) {
+            String string = buf.readString();
+            CraftingRecipeCategory craftingRecipeCategory = buf.readEnumConstant(CraftingRecipeCategory.class);
+            RawShapedRecipe rawShapedRecipe = RawShapedRecipe.PACKET_CODEC.decode(buf);
+            boolean bl = buf.readBoolean();
+            return new EndAnchorBlockCraftingRecipe(string, craftingRecipeCategory, rawShapedRecipe, bl);
+        }
+
+        private static void write(RegistryByteBuf buf, EndAnchorBlockCraftingRecipe recipe) {
+            buf.writeString(recipe.group);
+            buf.writeEnumConstant(recipe.category);
+            RawShapedRecipe.PACKET_CODEC.encode(buf, recipe.raw);
+            buf.writeBoolean(recipe.showNotification);
+        }
     }
 }
--- src/main/java/sh/sit/endanchor/SitsEndAnchor.java
+++ src/main/java/sh/sit/endanchor/SitsEndAnchor.java
@@ -1,8 +1,8 @@
 package sh.sit.endanchor;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.impl.util.log.LogCategory;
 import net.minecraft.block.entity.BlockEntityType;
-import net.minecraft.recipe.SpecialRecipeSerializer;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -11,7 +11,9 @@ public class SitsEndAnchor implements ModInitializer {
     public static final EndAnchorBlock END_ANCHOR_BLOCK = Registry.register(Registries.BLOCK, Identifier.of("endanchor", "end_anchor"), new EndAnchorBlock());
     public static final BlockEntityType<EndAnchorBlockEntity> END_ANCHOR_BLOCK_ENTITY = Registry.register(Registries.BLOCK_ENTITY_TYPE, Identifier.of("endanchor", "end_anchor_block_entity"), BlockEntityType.Builder.create(EndAnchorBlockEntity::new, END_ANCHOR_BLOCK).build());
     public static final EndAnchorBlockItem END_ANCHOR_BLOCK_ITEM = Registry.register(Registries.ITEM, Identifier.of("endanchor", "end_anchor"), new EndAnchorBlockItem());
-    public static final SpecialRecipeSerializer<EndAnchorBlockCraftingRecipe> END_ANCHOR_BLOCK_CRAFTING_RECIPE = Registry.register(Registries.RECIPE_SERIALIZER, Identifier.of("endanchor", "end_anchor_recipe"), new SpecialRecipeSerializer<>(EndAnchorBlockCraftingRecipe::new));
+    public static final EndAnchorBlockCraftingRecipe.Serializer END_ANCHOR_BLOCK_CRAFTING_RECIPE = Registry.register(Registries.RECIPE_SERIALIZER, Identifier.of("endanchor", "end_anchor_recipe"), new EndAnchorBlockCraftingRecipe.Serializer());
+
+    public static final LogCategory LOG_CATEGORY = LogCategory.create("SitsEndAnchor");
 
     @Override
     public void onInitialize() {
